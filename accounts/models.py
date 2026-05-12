9491bc0f90643fa3758053eab0014f95a65ff8f7
bahrouni-ayoub <Ayoub.BAHROUNI@esprit.tn>
2026-05-12 10:04:49 +0100

    Add user profile model with roles

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from config import settings
+
 
 # Create your models here.
+class Profile(models.Model):
+    ROLE_STUDENT = 'student'
+    ROLE_TUTOR = 'tutor'
+    
+    ROLE_CHOICES = [
+        (ROLE_STUDENT, 'étudiant'),
+        (ROLE_TUTOR, 'tuteur'),
+    ]
+    
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile"
+    )
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    students = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        related_name="tutors"
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_role_display()}"
