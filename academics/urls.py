5f53f3ef16c6d12b21b831008b1ad85279fbd1be
bahrouni-ayoub <Ayoub.BAHROUNI@esprit.tn>
2026-05-22 12:00:50 +0100

    Add note management routes

--- academics/urls.py
+++ academics/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("basket/", views.basket_view, name="basket"),
     path("basket/add/<int:module_id>/", views.add_module_view, name="add_module"),
     path("basket/remove/<int:module_choisi_id>/", views.remove_module_view, name="remove_module"),
+    path("notes/<int:module_choisi_id>/", views.manage_notes_view, name="manage_notes"),
 ]
